e17d137c74654acf16ebf6e7990e1567440f4c67
Yuvraj Dharmapuri <sree.deepthi2006@gmail.com>
2023-08-21 16:00:28 -0400

    Implemented Add button, Day 17

--- UdemyProjects/todos_graphical_user_interface.py
+++ UdemyProjects/todos_graphical_user_interface.py
@@ -2,10 +2,22 @@ import functions
 import PySimpleGUI as sg
 
 label = sg.Text("Enter a todo: ")
-input_box = sg.InputText(tooltip="Enter todo here")
+input_box = sg.InputText(tooltip="Enter todo here", key="todo")
 
 add_button = sg.Button("Add")
 
-window = sg.Window("Todo App", layout=[[label], [input_box, add_button]])
-window.read()
+window = sg.Window("Todo App",
+                   layout=[[label], [input_box, add_button]],
+                   font=20)
+while True:
+    event, values = window.read()
+    match event:
+        case "Add":
+            todos = functions.get_todos()
+            new_todo = values["todo"] + "\n"
+            todos.append(new_todo)
+            functions.write_todos(todos)
+        case sg.WIN_CLOSED:
+            break
+
 window.close()
